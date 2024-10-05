084f82506507de76572b8c6e74444ea58e3185db
Stacy-JoyM <jstacymuthoka@gmail.com>
2024-10-05 23:42:42 +0300

    Array methods

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Write your solution here!
+let cats = ["Milo", "Otis", "Garfield"];
+
+// let name = "Ralph"\
+function destructivelyAppendCat(name){
+    cats.push(name); 
+}
+destructivelyAppendCat("Ralph"); 
+
+// name = "Bob"
+function destructivelyPrependCat(name){
+    cats.unshift(name); 
+}
+destructivelyPrependCat("Bob"); 
+
+function destructivelyRemoveLastCat(){
+    cats.pop(); 
+}
+destructivelyRemoveLastCat()
+
+function destructivelyRemoveFirstCat(){
+    cats.shift(); 
+}
+destructivelyRemoveFirstCat()
+function appendCat(name){
+    let newCat = cats.concat(name)
+    return newCat;
+}
+appendCat("Broom")
+
+function prependCat(name){
+    let newCat = [name].concat(cats)
+    return newCat;
+}
+prependCat("Broom", cats)
+
+function removeLastCat(){
+    return cats.slice(0,-1)
+}
+removeLastCat()
+
+function removeFirstCat(){
+    return cats.slice(1)
+
+}
+removeFirstCat();
+
+
